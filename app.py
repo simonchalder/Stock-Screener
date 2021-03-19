3b27e0daf85b283a495a42454a044fc508ceb685
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-03-19 11:37:56 +0000

    Initial commit. Created API calls, filtered relevant data into class variables. Instansiated object holds relevant data

---
+++ app.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import requests
+
+class Stock:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        
+        self.overview = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol={0}&interval=5min&apikey=KEY'.format(symbol))
+        self.balance = requests.get('https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={0}&apikey=KEY'.format(symbol))
+        self.cash = requests.get('https://www.alphavantage.co/query?function=CASH_FLOW&symbol={0}&apikey=KEY'.format(symbol))
+        self.income = requests.get('https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={0}&apikey=KEY'.format(symbol))
+        # print(self.income.json())
+        self.name = self.overview.json()['Name']
+        self.cap = self.overview.json()['MarketCapitalization']
+        self.balance.json()['annualReports'][0]['totalLiabilities']
+        self.peRatio = self.overview.json()['PERatio']
+        self.eps = self.overview.json()['EPS']
+        self.cash = self.cash.json()['annualReports'][0]['operatingCashflow']
+        self.borrowings = self.balance.json()['annualReports'][0]['currentDebt']
+        self.shareholder_equity = self.balance.json()['annualReports'][0]['totalShareholderEquity']
+        self.annual_profits = self.income.json()['annualReports'][0]['comprehensiveIncomeNetOfTax']
+
+
+
+code = input("Enter stock code").upper()
+
+newStock = Stock(code)
+
+print(newStock.annual_profits)
