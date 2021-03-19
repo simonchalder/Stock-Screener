8498b8c9171bc8583cbbfe4aedcaeb1fb0990dab
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-03-19 12:01:04 +0000

    Added comments to app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+# Imports
+
 from dotenv import load_dotenv
 import requests
+
+# Class Definition
 
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
         
+        # API Calls
+
         self.overview = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol={0}&interval=5min&apikey=KEY'.format(symbol))
         self.balance = requests.get('https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={0}&apikey=KEY'.format(symbol))
         self.cash = requests.get('https://www.alphavantage.co/query?function=CASH_FLOW&symbol={0}&apikey=KEY'.format(symbol))
         self.income = requests.get('https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={0}&apikey=KEY'.format(symbol))
-        # print(self.income.json())
+
+        # Class variable assignment
+
         self.name = self.overview.json()['Name']
         self.cap = self.overview.json()['MarketCapitalization']
         self.balance.json()['annualReports'][0]['totalLiabilities']
@@ -20,7 +28,7 @@ class Stock:
         self.shareholder_equity = self.balance.json()['annualReports'][0]['totalShareholderEquity']
         self.annual_profits = self.income.json()['annualReports'][0]['comprehensiveIncomeNetOfTax']
 
-
+# Test code
 
 code = input("Enter stock code").upper()
 
